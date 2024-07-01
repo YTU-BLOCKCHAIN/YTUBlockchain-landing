5e420559623e9b4af2b001295b086b061fdfc883
Ahmed Zeyad <keyboord01@gmail.com>
2024-07-01 21:04:38 +0300

    init for translation

--- components/whoAreWe/IntroductionSection.tsx
+++ components/whoAreWe/IntroductionSection.tsx
@@ -1,16 +1,14 @@
-const IntroductionSection = () => (
-  <section className="text-center py-8 px-4">
-    <h2 className="text-4xl font-bold mb-4">Biz kimiz?</h2>
-    <p className="text-lg ">
-      YTU Blockchain topluluğu, 2022 yılında Yıldız teknik üniversitesinde
-      kuruldu. Kurulduğu günden bu yana, blockchain ve Web3 ekosistemleri
-      öncelikli olmak üzere bu alanlara ilgili ve kendini geliştirmek
-      isteyenlere fırsat tanıyan bir topluluğuz. Verdiğimiz eğitimler, organize
-      ettiğimiz etkinlikler başta olmak üzere neredeyse her günümüzü aktif
-      geçirirken, çekirdek bir aile olarak başladığımız bu yolda gün geçtikçe
-      ailemizi genişletiyor, beraber öğrenip beraber üretiyoruz.
-    </p>
-  </section>
-);
+import { useTranslations } from "next-intl";
+import React from "react";
+
+const IntroductionSection = () => {
+  const t = useTranslations("WhoAreWe");
+  return (
+    <section className="text-center py-8 px-4">
+      <h2 className="text-4xl font-bold mb-4">{t("whoAreWe")}</h2>
+      <p className="text-lg ">{t("description1")}</p>
+    </section>
+  );
+};
 
 export default IntroductionSection;
