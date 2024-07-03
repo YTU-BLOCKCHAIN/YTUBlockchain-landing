f4859d7396db6490e0e2a6c7afa34dd4e44de4bc
Ahmed Zeyad <keyboord01@gmail.com>
2024-07-03 18:06:59 +0300

    update first page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,6 +2,8 @@ import { useTranslations } from "next-intl";
 import { unstable_setRequestLocale } from "next-intl/server";
 import TwitterFeed from "@/components/Common/TwitterFeed";
 import Hero from "@/components/Hero";
+import IntroductionSection from "@/components/content/IntroductionSection";
+
 // import VideoSection from "@/components/VideoSection";
 
 type Props = {
@@ -15,13 +17,15 @@ export default function Index({ params: { locale } }: Props) {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="text-center mt-16">
-        <h1 className="text-5xl font-extrabold">{t("welcome")}</h1>
-        <p className="mt-6 text-xl text-gray-700">{t("description")}</p>
+      <div className="max-w-screen-2xl ">
+        <div className="text-center my-16">
+          <h1 className="text-5xl font-extrabold">{t("welcome")}</h1>
+        </div>
+        <IntroductionSection />
+        <Hero />
+        {/* <VideoSection /> */}
+        <TwitterFeed />
       </div>
-      <Hero />
-      {/* <VideoSection /> */}
-      <TwitterFeed />
     </main>
   );
 }
